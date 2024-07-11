b604da51ea0cbefaea0c5be42bba3bb98191b904
Wanchana <s.wanchana41@gmail.com>
2024-07-11 15:55:14 +0700

    fix: add xs sticky in order summary

--- client/src/components/cart_components/OrderSummary.jsx
+++ client/src/components/cart_components/OrderSummary.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function OrderSummary() {
   return (
-    <div className="w-full h-auto md:basis-1/3 border-solid border-purple-500 border-[2px]">
-      <div className="md:sticky md:top-0 bg-white border-solid border-[1px] border-[#D8D8D8] rounded-[8px] flex flex-col px-[24px] py-[16px]">
+    <div className="w-full h-auto md:basis-1/3 ">
+      <div className="fixed bottom-[72px] left-0 md:sticky md:top-0 w-full bg-white border-solid border-[1px] border-[#D8D8D8] rounded-[8px] flex flex-col px-[24px] py-[16px] overflow-hidden">
         <p>สรุปรายการ</p>
         <div className="summary-list flex flex-col">
           <div className="summary-detail border-solid border-b-[1px] pt-[16px] pb-[24px] flex flex-row justify-between">
