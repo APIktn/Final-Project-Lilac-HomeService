08416c1ea418ce174d811d4d5f339e02414a8d03
James <apisitamornktn@gmail.com>
2024-07-14 00:07:53 +0700

    fix:bug submit when no error at policy and terms button

--- client/src/pages/TechnicianAuthenticatedApp.jsx
+++ client/src/pages/TechnicianAuthenticatedApp.jsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage_technician from "./HomePage_technician";
 import NotFoundPage from "./NotFoundPage";
-import PolicyPage from "./PolicyPage";
-import TermsPage from "./TermsPage";
 
 function TechnicianAuthenticatedApp() {
   return (
     <div className="App">
       <Routes>
         <Route path="/technician" element={<HomePage_technician />} />
-        <Route path="/policy" element={<PolicyPage />} />
-        <Route path="/terms" element={<TermsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
