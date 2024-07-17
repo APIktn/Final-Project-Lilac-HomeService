4cced9f6a0f37469cbe42155043a120a70d6af9b
Wanchana <s.wanchana41@gmail.com>
2024-07-17 11:44:16 +0700

    fix: adjust stepbutton logic

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -4,15 +4,19 @@ import StepButtons from "../components/cart_components/stepper/StepButton";
 import React, { useState } from "react";
 
 export const CartContext = React.createContext();
+export const SummaryContext = React.createContext();
 
 function CartPage() {
   const [activeStep, setActiveStep] = useState(0);
+  const [netPrice, setNetPrice] = useState(0);
 
   return (
     <CartContext.Provider value={{ activeStep, setActiveStep }}>
       <Navbar_user />
-      <CartForm />
-      <StepButtons />
+      <SummaryContext.Provider value={{ netPrice, setNetPrice }}>
+        <CartForm />
+        <StepButtons />
+      </SummaryContext.Provider>
     </CartContext.Provider>
   );
 }
