23acb4ccdb469e84f339163df090597ddb630ef3
Wanchana <s.wanchana41@gmail.com>
2024-07-18 18:35:58 +0700

    fix: add stepbutton hover and active style

--- client/src/components/cart_components/stepper/StepButton.jsx
+++ client/src/components/cart_components/stepper/StepButton.jsx
@@ -72,7 +72,7 @@ export default function StepButtons() {
     <div className="bottom-navigator w-full h-[72px] md:h-[92px] bg-white border-solid border-[1px] border-t-gray-300 sticky bottom-0 z-10 overflow-hidden">
       <div className="container h-full mx-auto py-2 px-4 md:px-20 gap-4 flex justify-between items-center">
         <button
-          className="w-[164px] h-[40px] md:h-[44px] md:text-[16px] font-[500] text-center border-solid border-[1px] border-blue-600 rounded-[8px] text-blue-600"
+          className="w-[164px] h-[40px] md:h-[44px] md:text-[16px] font-[500] text-center border-solid border-[1px] border-blue-600 rounded-[8px] text-blue-600 hover:border-[#799FF7] hover:text-[#799FF7] active:border-[#0E3FB0] active:text-[#0E3FB0]"
           onClick={handleBack}
         >
           {"< ย้อนกลับ"}
@@ -81,7 +81,7 @@ export default function StepButtons() {
           className={`${
             isDisabled()
               ? "w-[164px] h-[40px] md:h-[44px] md:text-[16px] font-[500] text-center border-solid border-[1px] bg-gray-300 rounded-[8px] text-gray-100"
-              : "w-[164px] h-[40px] md:h-[44px] md:text-[16px] font-[500] text-center border-solid border-[1px] bg-blue-600 rounded-[8px] text-white"
+              : "w-[164px] h-[40px] md:h-[44px] md:text-[16px] font-[500] text-center border-solid border-[1px] bg-blue-600 rounded-[8px] text-white hover:bg-[#4C7FF4] active:bg-[#0E3FB0]"
           }`}
           onClick={handleNext}
           disabled={isDisabled()}
