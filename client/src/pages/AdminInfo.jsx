cc98729b1f73e9d876e7b029c7ae7192467571c9
benwata777 <benwata777@gmail.com>
2024-07-23 16:21:16 +0700

    feat: select mech amd status

--- client/src/pages/AdminInfo.jsx
+++ client/src/pages/AdminInfo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "../components/Navbar_user";
+import Navbar from "../components/Navbar_admin";
 import Footer from "../components/Footer";
 import CustomerInfoHeader from "../components/customer/CustomerInfoHeader";
 import AdminInfoBody from "../components/customer/AdminInfoBody";
