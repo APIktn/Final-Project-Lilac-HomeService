3e4b73d8eb8ac4688557631a0fde0dc08596c35b
Wanchana <s.wanchana41@gmail.com>
2024-07-25 02:11:01 +0700

    fix: replace service image hard code with fetch data from database

--- client/src/components/cart_components/ServiceImage.jsx
+++ client/src/components/cart_components/ServiceImage.jsx
@@ -1,15 +1,19 @@
-function ServiceImage() {
+function ServiceImage({ dataFromState }) {
+  const imageSrc = dataFromState[0] ? dataFromState[0].image : null;
+
   return (
-    <div className="absolute w-full z-0 top-0 left-0">
-      <div className="image-container w-full h-[168px] relative z-0 md:h-[240px]">
-        <div className="blend-color w-full h-full bg-[#163C93] opacity-40 absolute z-0"></div>
-        <img
-          className="service-image w-full h-full object-cover"
-          src="https://www.figma.com/file/0N9QuVAydHaNFReRpdWmME/image/0f615676862aef146f752c8fc736725679ace39d"
-          alt="AC-Cleaning-Picture"
-        />
+    imageSrc && (
+      <div className="absolute w-full z-0 top-0 left-0">
+        <div className="image-container w-full h-[168px] relative z-0 md:h-[240px]">
+          <div className="blend-color w-full h-full bg-[#163C93] opacity-40 absolute z-0"></div>
+          <img
+            className="service-image w-full h-full object-cover"
+            src={imageSrc}
+            alt="AC-Cleaning-Picture"
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
