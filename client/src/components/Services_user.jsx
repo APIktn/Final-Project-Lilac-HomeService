31171e6d7e22006436119e2228334f4d8a7daca9
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-08-02 18:33:35 +0700

    feat:done landing page

--- client/src/components/Services_user.jsx
+++ client/src/components/Services_user.jsx
@@ -2,50 +2,259 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ServiceCard_user from "./ServiceCard_user";
 import { useNavigate } from "react-router-dom";
+import Review1 from "../assets/images/review1.jpg";
+import Review2 from "../assets/images/review2.jpg";
+import Review3 from "../assets/images/review3.jpg";
+import Review4 from "../assets/images/review4.jpg";
+import Review5 from "../assets/images/review5.jpg";
 
 const Service_user = () => {
   const [services, setServices] = useState([]);
+  const [currentTestimonialIndex, setCurrentTestimonialIndex] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const itemsPerPage = 3; // Number of items to show per page
 
   const navigate = useNavigate();
+
   const handleMenuItemClick = (path) => {
     navigate(path);
   };
 
   const getServices = async () => {
-    const result = await axios.get(
-      `http://localhost:4000/services/top3services`
-    );
-    console.log(result.data.data);
-    setServices(result.data.data);
+    try {
+      const result = await axios.get(
+        `http://localhost:4000/services/top6services`
+      );
+      setServices(result.data.data);
+    } catch (error) {
+      console.error("Error fetching services:", error);
+    }
   };
 
   useEffect(() => {
     getServices();
   }, []);
 
-  let filteredServices = services;
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedServices = services.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    if (endIndex < services.length) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (startIndex > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const testimonials = [
+    {
+      id: 1,
+      name: "คุณน้ำฝน ทางสายธาร",
+      message:
+        "ต้องบอกว่าเราใช้บริการที่นี่ครบทุกอย่าง จริง ๆ ระบบการจัดการงานดีมาก ๆ ค่ะ",
+      image: Review1,
+    },
+    {
+      id: 2,
+      name: "คุณแอมแปร์ วิศารุธ",
+      message:
+        "ใช้บริการ HomService ติดแอร์ที่คาเฟ่ตลอดเพราะช่างเก็บงานละเอียดมาก ทำให้ Mode&Tone ของร้านไม่เสีย มืออาชีพมาก ๆ ครับ",
+      detail: "(เจ้าของกิจการ Ampere Cafe)",
+      image: Review2,
+    },
+    {
+      id: 3,
+      name: "คุณแป้ง ภัทรภร",
+      message:
+        "คือช่างเป็นมืออาชีพมาก มีการอัพเดทสถานะตลอดว่ากำลังเดินทางมา สุภาพมาก มาก่อนเวลานัดด้วยค่ะ แฮปปี้สุด ๆ",
+      detail: "",
+      image: Review3,
+    },
+    {
+      id: 4,
+      name: "เซฟ Riccardo Bonucci",
+      message:
+        "I always rely on HomeService for installations. Technician was incredibly friendly, and thanks to their expertise. ชอบ มาก ครับ",
+      detail: "(เจ้าของร้าน Made in Italian)",
+      image: Review4,
+    },
+    {
+      id: 5,
+      name: "คุณแก้ว ผ่องใส",
+      message:
+        "HomeServices ใช้งานได้ง่ายค่ะ ช่างดูแลเราดีมาก ชอบที่ทำงานเสร็จเรียบมากค่ะ",
+      detail: "",
+      image: Review5,
+    },
+  ];
+
+  const handleNext = () => {
+    setCurrentTestimonialIndex((prevIndex) =>
+      prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevious = () => {
+    setCurrentTestimonialIndex((prevIndex) =>
+      prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+    );
+  };
+
+  const getAdjacentIndices = () => {
+    const prevIndex =
+      currentTestimonialIndex === 0
+        ? testimonials.length - 1
+        : currentTestimonialIndex - 1;
+    const nextIndex =
+      currentTestimonialIndex === testimonials.length - 1
+        ? 0
+        : currentTestimonialIndex + 1;
+
+    return { prevIndex, nextIndex };
+  };
+
+  const { prevIndex, nextIndex } = getAdjacentIndices();
 
   return (
-    <section className="py-8 lg:py-16">
-      <div className="container mx-auto px-4">
-        <h2 className="text-[30px] lg:text-[32px] font-medium text-center text-blue-900 mb-8 ">
-          บริการยอดฮิตของเรา
-        </h2>
-        <div className="p-4 mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 xl:gap-8 md:px-20 md:py-16">
-          {filteredServices.map((service, index) => (
-            <ServiceCard_user
-              key={`${service.service_id}-${index}`}
-              service={service}
-            />
-          ))}
+    <section className="relative py-12 lg:py-8 overflow-hidden">
+      {/* Video Background */}
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0 hidden md:block" // Hide on mobile
+        src="https://res.cloudinary.com/dikwoz5gt/video/upload/f_auto:video,q_auto/j6p2j1reqkdcqdaemxmc"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+
+      {/* Blue Overlay */}
+      <div className="absolute top-0 left-0 w-full h-full md:bg-blue-300 opacity-50 z-0"></div>
+
+      {/* White Upper Background */}
+      <div className="absolute top-0 left-0 w-full h-[10%] bg-white z-0"></div>
+
+      <div className="relative z-10">
+        <div className="container mx-auto px-6 py-6  lg:px-8 ">
+          <h2 className="text-3xl lg:text-4xl font-medium text-center text-blue-900 mb-12">
+            บริการยอดฮิตของเรา
+          </h2>
+          {/* Grid and Pagination Buttons */}
+          <div className="relative">
+            {/* Grid */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-12">
+              {displayedServices.map((service, index) => (
+                <ServiceCard_user
+                  key={`${service.service_id}-${index}`}
+                  service={service}
+                />
+              ))}
+            </div>
+
+            {/* Pagination Buttons */}
+
+            <div className="flex justify-between">
+              {/* Previous Button */}
+              {currentPage > 0 && (
+                <div className="flex justify-between mt-8 absolute  lg:-left-7 lg:right-0 lg:top-28 px-0 max-sm:hidden lg:block">
+                  <button
+                    onClick={handlePreviousPage}
+                    disabled={currentPage === 0}
+                    className="px-4 py-2 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-medium rounded-full shadow-lg hover:shadow-xl transition duration-300 ease-in-out max-sm:hidden lg:block transform hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed"
+                  >
+                    &lt;
+                  </button>
+                </div>
+              )}
+
+              {/* Next Button */}
+              {endIndex < services.length && (
+                <div className="flex justify-between mt-8 absolute  lg:left-[98%] lg:right-0 lg:top-28 px-0 max-sm:hidden lg:block">
+                  <button
+                    onClick={handleNextPage}
+                    disabled={endIndex >= services.length}
+                    className="px-4 py-2 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-medium rounded-full shadow-lg hover:shadow-xl transition duration-300 ease-in-out sm:hidden lg:block transform hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed "
+                  >
+                    &gt;
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="flex justify-center mt-10 lg:mt-12">
+            <button
+              onClick={() => handleMenuItemClick("/servicelist")}
+              className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 transition duration-300 ease-in-out"
+            >
+              ดูบริการทั้งหมด
+            </button>
+          </div>
         </div>
-        <div className="text-center mt-6 lg:mt-8">
-          <button
-            onClick={() => handleMenuItemClick("/servicelist")}
-            className="px-4 lg:px-6 py-2 bg-blue-600 text-white rounded"
-          >
-            ดูบริการทั้งหมด
-          </button>
+
+        {/* Testimonial Section */}
+        <div className="bg-white mt-12 py-8">
+          <div className="container mx-auto px-6 lg:px-8 text-center">
+            <h3 className="text-2xl font-semibold text-blue-900 mb-6">
+              รีวิวจริงจากลูกค้าของเรา ⭐️
+            </h3>
+            <div className="flex items-center justify-center space-x-4">
+              <button
+                onClick={handlePrevious}
+                className="bg-blue-500 text-white px-3 py-1 rounded-full hover:bg-blue-600 transition"
+              >
+                &lt;
+              </button>
+
+              {/* Show only current testimonial on mobile */}
+              <div className="hidden md:flex flex-col items-center opacity-50 transition duration-300 ease-in-out">
+                <img
+                  src={testimonials[prevIndex].image}
+                  alt={testimonials[prevIndex].name}
+                  className="w-24 h-36 rounded-lg mb-2 object-cover"
+                />
+                <p className="text-sm">{testimonials[prevIndex].name}</p>
+              </div>
+
+              {/* Current Testimonial */}
+              <div className="flex flex-col items-center w-full h-[350px] max-w-md p-4 border border-gray-200 rounded-lg  transition duration-300 ease-in-out">
+                <img
+                  src={testimonials[currentTestimonialIndex].image}
+                  alt={testimonials[currentTestimonialIndex].name}
+                  className="w-32 h-48 rounded-lg mb-4 object-cover  bg-slate-50"
+                />
+                <p className="text-[12px] lg:text-[16px] lg:w-[400px] italic">
+                  "{testimonials[currentTestimonialIndex].message}"
+                </p>
+                <p className="mt-4 text-[10px] lg:text-sm font-medium text-blue-900">
+                  - {testimonials[currentTestimonialIndex].name}{" "}
+                  {testimonials[currentTestimonialIndex].detail}
+                </p>
+              </div>
+
+              {/* Show only next testimonial on mobile */}
+              <div className="hidden md:flex flex-col items-center opacity-50 transition duration-300 ease-in-out">
+                <img
+                  src={testimonials[nextIndex].image}
+                  alt={testimonials[nextIndex].name}
+                  className="w-24 h-36 rounded-lg mb-2 object-cover"
+                />
+                <p className="text-sm">{testimonials[nextIndex].name}</p>
+              </div>
+
+              <button
+                onClick={handleNext}
+                className="bg-blue-500 text-white px-3 py-1 rounded-full hover:bg-blue-600 transition"
+              >
+                &gt;
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- server/routes/servicecards.mjs
+++ server/routes/servicecards.mjs
@@ -4,26 +4,26 @@ import supabase from "../utils/db.mjs";
 const servicesRouter = Router();
 
 //-------Get Top Services------//
-servicesRouter.get("/top3services", async (req, res) => {
+servicesRouter.get("/top6services", async (req, res) => {
   try {
     // Extract query parameters for filtering
     const { lowerprice, higherprice, top } = req.query;
 
     // Initialize the base query to get top services based on quantity
     let baseQuery = supabase
-      .from('service_list')
-      .select('service_id, quantity')
-      .order('quantity', { ascending: false });
+      .from("service_list")
+      .select("service_id, quantity")
+      .order("quantity", { ascending: false });
 
     // Apply filters based on the provided price range
     if (top) {
-      baseQuery = baseQuery.gte('quantity', parseFloat(top));
+      baseQuery = baseQuery.gte("quantity", parseFloat(top));
     }
     if (lowerprice) {
-      baseQuery = baseQuery.gte('price', parseFloat(lowerprice));
+      baseQuery = baseQuery.gte("price", parseFloat(lowerprice));
     }
     if (higherprice) {
-      baseQuery = baseQuery.lte('price', parseFloat(higherprice));
+      baseQuery = baseQuery.lte("price", parseFloat(higherprice));
     }
 
     // Execute the base query to get service IDs
@@ -42,12 +42,13 @@ servicesRouter.get("/top3services", async (req, res) => {
     }
 
     // Extract service IDs
-    const serviceIds = baseData.map(item => item.service_id);
+    const serviceIds = baseData.map((item) => item.service_id);
 
     // Initialize the query to get detailed service data based on service IDs
     let detailedQuery = supabase
-      .from('services')
-      .select(`
+      .from("services")
+      .select(
+        `
         service_id,
         image, 
         service_name,       
@@ -59,8 +60,9 @@ servicesRouter.get("/top3services", async (req, res) => {
         categories (
           category_name
         )
-      `)
-      .in('service_id', serviceIds);
+      `
+      )
+      .in("service_id", serviceIds);
 
     // Execute the detailed query
     let { data: detailedData, error: detailedError } = await detailedQuery; // Note the use of 'let' instead of 'const' here
@@ -71,9 +73,124 @@ servicesRouter.get("/top3services", async (req, res) => {
 
     // Calculate the total quantity for all services and sort by total_quantity
     if (detailedData) {
-      detailedData.forEach(service => {
+      detailedData.forEach((service) => {
         if (Array.isArray(service.service_list)) {
-          service.total_quantity = service.service_list.reduce((sum, item) => sum + item.quantity, 0);
+          service.total_quantity = service.service_list.reduce(
+            (sum, item) => sum + item.quantity,
+            0
+          );
+        } else {
+          service.total_quantity = 0;
+        }
+      });
+
+      // Sort by total_quantity descending
+      detailedData.sort((a, b) => b.total_quantity - a.total_quantity);
+
+      // Limit to top 3 services by total_quantity
+      detailedData = detailedData.slice(0, 6); // Reassigning 'detailedData' is causing the error
+
+      // Calculate totalQuantity for the top 3 services
+      let totalQuantity = detailedData.reduce(
+        (sum, service) => sum + service.total_quantity,
+        0
+      );
+
+      return res.status(200).json({
+        message: "Successfully retrieved the list of top services.",
+        data: detailedData,
+        totalQuantity: totalQuantity,
+      });
+    } else {
+      return res.status(200).json({
+        message: "No services found matching the criteria.",
+        data: [],
+        totalQuantity: 0,
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching services:", error.message);
+
+    return res.status(500).json({
+      message: "Server could not retrieve services due to a database error.",
+    });
+  }
+});
+servicesRouter.get("/top3services", async (req, res) => {
+  try {
+    // Extract query parameters for filtering
+    const { lowerprice, higherprice, top } = req.query;
+
+    // Initialize the base query to get top services based on quantity
+    let baseQuery = supabase
+      .from("service_list")
+      .select("service_id, quantity")
+      .order("quantity", { ascending: false });
+
+    // Apply filters based on the provided price range
+    if (top) {
+      baseQuery = baseQuery.gte("quantity", parseFloat(top));
+    }
+    if (lowerprice) {
+      baseQuery = baseQuery.gte("price", parseFloat(lowerprice));
+    }
+    if (higherprice) {
+      baseQuery = baseQuery.lte("price", parseFloat(higherprice));
+    }
+
+    // Execute the base query to get service IDs
+    const { data: baseData, error: baseError } = await baseQuery;
+    if (baseError) {
+      console.error("Base query error:", baseError.message);
+      throw baseError; // This is not causing "Assignment to constant variable" error
+    }
+
+    if (baseData.length === 0) {
+      return res.status(200).json({
+        message: "No services found matching the criteria.",
+        data: [],
+        totalQuantity: 0,
+      });
+    }
+
+    // Extract service IDs
+    const serviceIds = baseData.map((item) => item.service_id);
+
+    // Initialize the query to get detailed service data based on service IDs
+    let detailedQuery = supabase
+      .from("services")
+      .select(
+        `
+        service_id,
+        image, 
+        service_name,       
+        category_id, 
+        service_list (
+          price,
+          quantity
+        ), 
+        categories (
+          category_name
+        )
+      `
+      )
+      .in("service_id", serviceIds);
+
+    // Execute the detailed query
+    let { data: detailedData, error: detailedError } = await detailedQuery; // Note the use of 'let' instead of 'const' here
+    if (detailedError) {
+      console.error("Detailed query error:", detailedError.message);
+      throw detailedError;
+    }
+
+    // Calculate the total quantity for all services and sort by total_quantity
+    if (detailedData) {
+      detailedData.forEach((service) => {
+        if (Array.isArray(service.service_list)) {
+          service.total_quantity = service.service_list.reduce(
+            (sum, item) => sum + item.quantity,
+            0
+          );
         } else {
           service.total_quantity = 0;
         }
@@ -86,7 +203,10 @@ servicesRouter.get("/top3services", async (req, res) => {
       detailedData = detailedData.slice(0, 3); // Reassigning 'detailedData' is causing the error
 
       // Calculate totalQuantity for the top 3 services
-      let totalQuantity = detailedData.reduce((sum, service) => sum + service.total_quantity, 0);
+      let totalQuantity = detailedData.reduce(
+        (sum, service) => sum + service.total_quantity,
+        0
+      );
 
       return res.status(200).json({
         message: "Successfully retrieved the list of top services.",
@@ -111,10 +231,11 @@ servicesRouter.get("/top3services", async (req, res) => {
 //-------Get All------//
 servicesRouter.get("/serviceslist", async (req, res) => {
   try {
-    const { lowerprice, higherprice, top } = req.query;   
+    const { lowerprice, higherprice, top } = req.query;
     let baseQuery = supabase
-      .from('services')
-      .select(`
+      .from("services")
+      .select(
+        `
         service_id,
         image,
         service_name,
@@ -122,35 +243,34 @@ servicesRouter.get("/serviceslist", async (req, res) => {
         categories (
           category_name
         )
-      `)
-      .order('service_id', { ascending: true }); 
-   
+      `
+      )
+      .order("service_id", { ascending: true });
+
     if (lowerprice) {
-      baseQuery = baseQuery.gt('service_list:price', parseFloat(lowerprice));
+      baseQuery = baseQuery.gt("service_list:price", parseFloat(lowerprice));
     }
     if (higherprice) {
-      baseQuery = baseQuery.lt('service_list:price', parseFloat(higherprice));
+      baseQuery = baseQuery.lt("service_list:price", parseFloat(higherprice));
     }
-   
+
     const { data, error } = await baseQuery;
     if (error) {
       throw error;
     }
-    
-    data.forEach(service => {
+
+    data.forEach((service) => {
       service.service_list.sort((a, b) => a.price - b.price);
     });
 
-    
     return res.status(200).json({
       message: "Successfully retrieved and sorted the list of top services.",
-      data: data
+      data: data,
     });
-
   } catch (error) {
     console.error("Error retrieving services:", error.message);
     return res.status(500).json({
-      message: "Server could not retrieve services due to a database error."
+      message: "Server could not retrieve services due to a database error.",
     });
   }
 });
