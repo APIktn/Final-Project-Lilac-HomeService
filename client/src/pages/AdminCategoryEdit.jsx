9cf00abe735483568a6d178fc4bafb8d0fbbab2b
Amvisarut <visarut.thk@gmail.com>
2024-08-13 23:51:27 +0700

    fix:UI

--- client/src/pages/AdminCategoryEdit.jsx
+++ client/src/pages/AdminCategoryEdit.jsx
@@ -274,7 +274,7 @@ function AdminCategoryEdit() {
                 {createCategory && (
                  <div className="flex flex-col gap-7 -mx-4 rounded-t-none mb-4 mt-10 pb-4 h-[180px] w-[380] pt-10 bg-white rounded-lg">
                  <div className="">                    
-                   <hr className="border-t-2 border-[#CCD0D7] w-[1235px] ml-5 -mt-10 mb-14" />
+                   <hr className="border-t-2 border-[#CCD0D7] w-[1350px] ml-5 -mt-10 mb-14" />
                    <div>
                    <span className="block ml-5 font-medium text-[16px] text-[#646C80] ">
                       สร้างเมื่อ
@@ -312,7 +312,7 @@ function AdminCategoryEdit() {
                   </div>
                   <div className="flex flex-col gap-7 -mx-4 mb-4 mt-10 pb-4 h-[180px] w-[380] pt-10 bg-white rounded-lg">
                   <div className="">                    
-                    <hr className="border-t-2 border-[#CCD0D7] w-[1235px] ml-5 -mt-10 mb-14" />
+                    <hr className="border-t-2 border-[#CCD0D7] w-[1350px] ml-5 -mt-10 mb-14" />
                     <div>
                     <span className="block ml-5 font-medium text-[16px] text-[#646C80] ">
                        สร้างเมื่อ
